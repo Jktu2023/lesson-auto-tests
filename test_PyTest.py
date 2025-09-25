188ca4d3529bb12796bafd805d2401e2eafc418a
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-09-25 14:56:27 +0700

    221

--- test_PyTest.py
+++ test_PyTest.py
@@ -4,6 +4,7 @@ from task_PyTest import count_vowels
 @pytest.mark.parametrize("number, expected", [
    ('hello', 2),
    ('apache', 3),
+   ('scktch', 0),
    ('Грозный', 2),
    ('Площадь революции', 7),
    ('Молчалин - герой нашего времени', 11), ])
