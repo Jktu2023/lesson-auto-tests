1c3c5a33489b9e52463ebd2d669559aa095e1ab8
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-10-02 14:46:03 +0700

    22

--- testAPI.py
+++ testAPI.py
@@ -6,6 +6,21 @@ import pytest
 from taskAPI import get_random_cat_image
 
 def test_get_random_cat_image(mocker):
-    mock.get = mocker.patch('taskAPI.requests.get')
-    mock.get.return_value.status_code = 200
-    mock.get.return_value.json.return_value = [{"url": "https://example.com/cat.jpg"}]
+    mock_get = mocker.patch('taskAPI.requests.get')
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = [{"url": "https://example.com/cat.jpg"}]
+
+    result = get_random_cat_image()
+    assert result == "https://example.com/cat.jpg"
+
+    # mock_get.return_value.status_code = 404
+    # result = get_random_cat_image()
+    # assert result is None
+
+def test_get_random_cat_image_error(mocker):
+    mock_get = mocker.patch('taskAPI.requests.get')
+    mock_get.return_value.status_code = 500
+
+    result = get_random_cat_image()
+    assert result is None
+
