0f519f75d67dc722ca47ee0f757ce89f1bdb6aa2
Jktu2023 <161673133+Jktu2023@users.noreply.github.com>
2025-10-17 13:00:23 +0700

    11

--- OD02_algor.py
+++ OD02_algor.py
@@ -91,3 +91,137 @@ print('Сейчас первый элемент очереди', queue1.peek())
 print('Очередь пустая?', queue1.is_empty())
 print('Состояние очереди', queue1.display())
 
+class Graph: # граф
+    def __init__(self): # конструктор графа
+        self.graph = {} # словарь графа
+
+    def add_vertex(self, vertex): # добавление вершины
+        if vertex not in self.graph:
+            self.graph[vertex] = [] #
+
+    def add_edge(self, vertex1, vertex2): # добавление ребра
+        if vertex1 in self.graph and vertex2 in self.graph:
+            self.graph[vertex1].append(vertex2)
+            self.graph[vertex2].append(vertex1)
+
+    def get_neighbors(self, vertex): #
+        if vertex in self.graph:
+            return self.graph[vertex]
+        else:
+            return []
+
+    def display(self):
+        return self.graph
+
+graph1 = Graph()
+graph1.add_vertex('A')
+graph1.add_vertex('B')
+graph1.add_vertex('C')
+graph1.add_edge('A', 'B')
+graph1.add_edge('B', 'C')
+graph1.add_edge('C', 'A')
+
+print()
+print('Состояние графа', graph1.display())
+print('Соседи вершины A', graph1.get_neighbors('A'))
+print('Соседи вершины B', graph1.get_neighbors('B'))
+print('Соседи вершины C', graph1.get_neighbors('C'))
+
+
+class Tree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if self.value:
+            if value < self.value:
+                if self.left is None:
+                    self.left = Tree(value)
+                else:
+                    self.left.insert(value)
+            elif value > self.value:
+                if self.right is None:
+                    self.right = Tree(value)
+                else:
+                    self.right.insert(value)
+        else:
+            self.value = value
+
+    def search(self, value):
+        if self.value == value:
+            return True
+        if value < self.value:
+            if self.left:
+                return self.left.search(value)
+            return False
+        if value > self.value:
+            if self.right:
+                return self.right.search(value)
+            return False
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self): # вывод дерева
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = f'{self.value}'
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = f'{self.value}'
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = f'{self.value}'
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = f'{self.value}'
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+tree1 = Tree(5)
+tree1.insert(3)
+tree1.insert(7)
+tree1.insert(2)
+tree1.insert(4)
+tree1.insert(6)
+tree1.insert(8)
+
+print()
+print('Состояние дерева', tree1.display())
+print('Поиск значения 4', tree1.search(4))
+print('Поиск значения 9', tree1.search(9))
+print('Состояние дерева', tree1.display())
+
---
+++ OD03_alg_sort.py
@@ -0,0 +1,91 @@
+# Реализуйте один или несколько видов алгоритмов сортировки.
+
+import random
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    return arr
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [random.randint(0, 100) for _ in range(10)]
+print("Исходный массив:", arr)
+
+sorted_arr = bubble_sort(arr.copy())
+print("Пузырьковая сортировка:", sorted_arr)
+
+sorted_arr = insertion_sort(arr.copy())
+print("Сортировка вставками:", sorted_arr)
+
+sorted_arr = selection_sort(arr.copy())
+print("Сортировка выбором:", sorted_arr)
+
+sorted_arr = merge_sort(arr.copy())
+print("Сортировка слиянием:", sorted_arr)
+
+sorted_arr = quick_sort(arr.copy())
+print("Быстрая сортировка:", sorted_arr)
+
